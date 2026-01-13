bbacc6d03b8fa2024af8c32a21fecb14f52d2dc2
joshua Oyewole <joshpp2013@gmail.com>
2026-01-13 15:30:30 +0100

    fix- added domain to cookies config object

--- controllers/authController.js
+++ controllers/authController.js
@@ -37,6 +37,7 @@ const sendTokenResponse = (user, statusCode, res) => {
 
   res.cookie("token", token, {
     httpOnly: true,
+    domain: `.flysolarstore.com `,
     secure: process.env.NODE_ENV === "production" ? true : false,
     sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
     maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
@@ -502,7 +503,7 @@ const updateProfile = catchAsync(async (req, res, next) => {
   res.status(200).json({
     success: true,
     data: {
-      user:{
+      user: {
         _id: user._id,
         firstName: user.firstName,
         lastName: user.lastName,
@@ -601,6 +602,7 @@ const googleCallback = catchAsync(async (req, res) => {
     res.cookie("token", token, {
       httpOnly: true,
       secure: process.env.NODE_ENV === "production",
+      domain: `.flysolarstore.com`,
       sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
       maxAge: 7 * 24 * 60 * 60 * 1000,
       path: "/",
