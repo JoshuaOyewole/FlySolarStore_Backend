3f558c9fcea4851b754e37eaf05c947fc6f5589d
joshua Oyewole <joshpp2013@gmail.com>
2026-01-13 15:44:35 +0100

    fix

--- controllers/authController.js
+++ controllers/authController.js
@@ -37,7 +37,7 @@ const sendTokenResponse = (user, statusCode, res) => {
 
   res.cookie("token", token, {
     httpOnly: true,
-    domain: `.flysolarstore.com `,
+    domain: ".flysolarstore.com",
     secure: process.env.NODE_ENV === "production" ? true : false,
     sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
     maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
